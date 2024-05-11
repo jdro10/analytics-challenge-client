27bf9d7c708e4de1d38f36ca439acee1a3a3b127
Jorge Oliveira <jorgedroliveira10@gmail.com>
2024-05-11 14:50:41 +0100

    added option to copy current page link to clipboard; small UI spacing fix;

--- analytics-challenge/src/app/toolbar/toolbar.component.ts
+++ analytics-challenge/src/app/toolbar/toolbar.component.ts
@@ -2,12 +2,21 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css',
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  
+  constructor(private clipboard: Clipboard) {}
+
+  copyCurrentPageLinkToClickboard() {
+    this.clipboard.copy(window.location.href);
+  }
+}
