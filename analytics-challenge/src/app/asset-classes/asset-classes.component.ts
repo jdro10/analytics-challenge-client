62892ac628e06cbc8ec75e469dd47e3dd491daf0
Jorge Oliveira <jorgedroliveira10@gmail.com>
2024-05-12 16:42:22 +0100

    refactored scenario space component

---
+++ analytics-challenge/src/app/asset-classes/asset-classes.component.ts
@@ -0,0 +1,144 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '../dialog/dialog.component';
+import { SimulationParametersRequest } from '../models/SimulationParametersRequest';
+import { ScenarioSpace } from '../models/ScenarioSpace';
+import { SimulationService } from '../services/simulation.service';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { SpinnerComponent } from '../spinner/spinner.component';
+import { LinearChartComponent } from '../linear-chart/linear-chart.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+
+@Component({
+  selector: 'app-asset-classes',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    CommonModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    SpinnerComponent,
+    LinearChartComponent,
+    MatDividerModule,
+    MatListModule,
+  ],
+  templateUrl: './asset-classes.component.html',
+  styleUrl: './asset-classes.component.css',
+})
+export class AssetClassesComponent {
+  // asset classes initial allocation default value
+  readonly DEFAULT_ASSET_CLASS_VALUE = 10000;
+
+  // ui state variables
+  isDialogOpen: boolean = false;
+  loadingChart: boolean = false;
+  showLinearChart: boolean = false;
+
+  @Input() simulationData: any;
+  @Input() assetClasses: { [key: string]: number } = {};
+  @Input() selectedScenarioSpace: string = '';
+
+  simulationParametersRequest = {} as SimulationParametersRequest;
+  scenarioSpaces: ScenarioSpace = new ScenarioSpace();
+
+  simulationStats: { [key: string]: number } = {};
+
+  constructor(
+    public dialog: MatDialog,
+    private simulationService: SimulationService
+  ) {
+    Object.keys(this.scenarioSpaces.assetClasses).forEach((key) => {
+      this.simulationStats[key] = 0;
+    });
+  }
+
+  // Dialog shown when user enters invalid input in asset clases (e.g. characters or negative numbers)
+  invalidUserInputDialog(assetValue: number | null, key: string): void {
+    if (
+      (assetValue === null || isNaN(assetValue) || assetValue < 0) &&
+      !this.isDialogOpen
+    ) {
+      this.isDialogOpen = true;
+
+      const dialogRef = this.dialog.open(DialogComponent, {
+        data: {
+          title: 'Warning',
+          message: `Only positive numeric numbers can be used! Non valid inputs will be converted to the default value of ${this.DEFAULT_ASSET_CLASS_VALUE}. `,
+        },
+      });
+
+      // revert to asset class to default value
+      this.assetClasses[key] = this.DEFAULT_ASSET_CLASS_VALUE;
+
+      dialogRef.afterClosed().subscribe(() => {
+        this.isDialogOpen = false;
+      });
+    }
+  }
+
+  // This function performs a call to the custom API given the input asset classes
+  simulate(): void {
+    // ui state flags
+    this.loadingChart = true;
+    this.showLinearChart = false;
+
+    // set asset classes and selected scenario space
+    this.simulationParametersRequest = {
+      assetClasses: this.assetClasses,
+      scenarioSpace:
+        this.scenarioSpaces.assetClasses[this.selectedScenarioSpace],
+    };
+
+    // call the API and subscribe the result
+    this.simulationService
+      .performSimulation(this.simulationParametersRequest)
+      .subscribe(
+        (data) => {
+          this.simulationData = data;
+          this.simulationStats[this.selectedScenarioSpace] += 1;
+
+          if (data !== null) {
+            this.showLinearChart = true;
+          } else {
+            this.showDialog(
+              'Warning',
+              'No data was returned by the API for the given parameters! No chart will be shown. Please try again.'
+            );
+          }
+
+          this.loadingChart = false;
+        },
+        () => {
+          this.loadingChart = false;
+          this.showDialog(
+            'Error',
+            'Could not establish communication with simulation API.'
+          );
+        }
+      );
+  }
+
+  // Generic dialog with custom title and message
+  showDialog(title: string, message: string): void {
+    this.isDialogOpen = true;
+
+    const dialogRef = this.dialog.open(DialogComponent, {
+      data: {
+        title: title,
+        message: message,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      this.isDialogOpen = false;
+    });
+  }
+}
--- analytics-challenge/src/app/scenario-space/scenario-space.component.ts
+++ analytics-challenge/src/app/scenario-space/scenario-space.component.ts
@@ -1,35 +1,23 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { ScenarioSpaceService } from '../services/scenario-space.service';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from '../spinner/spinner.component';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from '../dialog/dialog.component';
-import { MatButtonModule } from '@angular/material/button';
-import { SimulationService } from '../services/simulation.service';
-import { LinearChartComponent } from '../linear-chart/linear-chart.component';
-import { SimulationParametersRequest } from './../models/SimulationParametersRequest';
 import { ScenarioSpace } from '../models/ScenarioSpace';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatListModule } from '@angular/material/list';
+import { AssetClassesComponent } from '../asset-classes/asset-classes.component';
 
 @Component({
   selector: 'app-scenario-space',
   standalone: true,
   imports: [
-    MatFormFieldModule,
     MatSelectModule,
-    MatInputModule,
     FormsModule,
     CommonModule,
     SpinnerComponent,
-    MatButtonModule,
-    LinearChartComponent,
-    MatDividerModule,
-    MatListModule,
+    AssetClassesComponent,
   ],
   templateUrl: './scenario-space.component.html',
   styleUrl: './scenario-space.component.css',
@@ -42,25 +30,17 @@ export class ScenarioSpaceComponent {
   loading: boolean = false;
   error: boolean = false;
   isDialogOpen: boolean = false;
-  loadingChart: boolean = false;
-  showLinearChart: boolean = false;
 
   // data variables
   selectedScenarioSpace: string = '';
   assetClasses: { [key: string]: number } = {};
   scenarioSpaces: ScenarioSpace = new ScenarioSpace();
-  simulationData = {};
-  simulationParametersRequest = {} as SimulationParametersRequest;
-  simulationStats: { [key: string]: number } = {};
 
   constructor(
     private scenarioSpaceService: ScenarioSpaceService,
-    private simulationService: SimulationService,
     public dialog: MatDialog
   ) {
-    Object.keys(this.scenarioSpaces.assetClasses).forEach((key) => {
-      this.simulationStats[key] = 0;
-    });
+
   }
 
   // This function retrieves the available asset classes for a specific scenario space
@@ -68,92 +48,38 @@ export class ScenarioSpaceComponent {
     // ui state flags
     this.loading = true;
     this.error = false;
-    this.showLinearChart = false;
     this.assetClasses = {};
 
     // call the API to get the available asset classes
     this.scenarioSpaceService
       .getScenarioSpace(this.selectedScenarioSpace)
-      .subscribe((data) => {
-        try {
-          const assetClassesJsonResponse = JSON.parse(JSON.stringify(data));
+      .subscribe({
+        next: (data) => {
+          try {
+            const assetClassesJsonResponse = JSON.parse(JSON.stringify(data));
 
-          Object.keys(assetClassesJsonResponse.asset_classes).forEach(
-            (key) => (this.assetClasses[key] = this.DEFAULT_ASSET_CLASS_VALUE)
-          );
-        } catch (error) {
-          this.error = true;
-          this.showDialog(
-            'Warning',
-            `The selected scenario scene (${this.selectedScenarioSpace}) has no asset classes. Please select a different option.`
-          );
-        } finally {
-          this.loading = false;
-        }
-      });
-  }
-
-  // This function performs a call to the custom API given the input asset classes
-  simulate(): void {
-    // ui state flags
-    this.loadingChart = true;
-    this.showLinearChart = false;
-
-    // set asset classes and selected scenario space
-    this.simulationParametersRequest = {
-      assetClasses: this.assetClasses,
-      scenarioSpace:
-        this.scenarioSpaces.assetClasses[this.selectedScenarioSpace],
-    };
-
-    // call the API and subscribe the result
-    this.simulationService
-      .performSimulation(this.simulationParametersRequest)
-      .subscribe(
-        (data) => {
-          this.simulationData = data;
-          this.simulationStats[this.selectedScenarioSpace] += 1;
-
-          if (data !== null) {
-            this.showLinearChart = true;
-          } else {
+            Object.keys(assetClassesJsonResponse.asset_classes).forEach(
+              (key) => (this.assetClasses[key] = this.DEFAULT_ASSET_CLASS_VALUE)
+            );
+          } catch (error) {
+            this.error = true;
             this.showDialog(
               'Warning',
-              'No data was returned by the API for the given parameters! No chart will be shown. Please try again.'
+              `The selected scenario scene (${this.selectedScenarioSpace}) has no asset classes. Please select a different option.`
             );
+          } finally {
+            this.loading = false;
           }
-
-          this.loadingChart = false;
         },
-        () => {
-          this.loadingChart = false;
+        error: () => {
+          this.loading = false;
+          this.error = true;
           this.showDialog(
             'Error',
-            'Could not establish communication with simulation API.'
+            `An unexpected error occurred while attempting to communicate with the API.`
           );
-        }
-      );
-  }
-
-  // Dialog shown when user enters invalid input in asset clases (e.g. characters or negative numbers)
-  invalidUserInputDialog(assetValue: number | null, key: string): void {
-    if ((assetValue === null || isNaN(assetValue) || assetValue < 0) && !this.isDialogOpen) {
-      this.isDialogOpen = true;
-
-      const dialogRef = this.dialog.open(DialogComponent, {
-        data: {
-          title: 'Warning',
-          message: `Only positive numeric numbers can be used! Non valid inputs will be converted to the default value of ${this.DEFAULT_ASSET_CLASS_VALUE}. `,
         },
       });
-
-      // revert to asset class to default value
-      this.assetClasses[key] = this.DEFAULT_ASSET_CLASS_VALUE;
-
-      dialogRef.afterClosed().subscribe(() => {
-        this.isDialogOpen = false;
-      });
-    }
   }
 
   // Generic dialog with custom title and message
